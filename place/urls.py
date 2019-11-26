ae3c337fd67e6a3fbaabddf6d8f654f022792635
JangHyukJin <wkdgurwls00@naver.com>
2019-11-26 16:17:47 +0900

    modifyied urls

--- place/urls.py
+++ place/urls.py
@@ -6,5 +6,7 @@ app_name = 'place'
 
 urlpatterns = [
     path('choice/', PlaceChoiceTemplateView.as_view(), name='choice'),
-    path('ListAPI', PlaceChoiceListView.as_view(), name='list_api'),
+    path('thanks/', ThanksTemplateView.as_view(), name='thanks'),
+    path('', PlaceChoiceListView.as_view(), name='list_api'),
+    path('UserProfileReceiveAPI', UserProfileReceiveView.as_view(), name='user_profile_receive')
 ]
