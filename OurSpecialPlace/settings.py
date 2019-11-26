a4905dd7c06ad16539ab93171f605d8fae757129
JungChaeMoon <jungchaemoon@gmail.com>
2019-11-26 16:35:17 +0900

    Refactoring

--- OurSpecialPlace/settings.py
+++ OurSpecialPlace/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/')
 ]
 
