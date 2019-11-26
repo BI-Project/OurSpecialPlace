5abf264c7ac2ad71b398bc6f7d7b740b97da638f
JangHyukJin <wkdgurwls00@naver.com>
2019-11-26 16:16:48 +0900

    added columns of serializer

--- place/serializer.py
+++ place/serializer.py
@@ -8,5 +8,16 @@ class PlaceSerializer(serializers.ModelSerializer):
         model = Place
         fields = (
             'picture',
-            'title',
+            'name',
+            'natural_city',
+            'static_dynamic',
+            'mountain_sea',
+            'history_modern',
+            'save_flex',
+            'accessibility',
+            'dormitory_hotel',
+            'season',
+            'day_N_night',
+            'age',
+            'comment',
         )
