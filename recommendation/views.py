c0f6472025272353e97897c4edb33e51a9f55158
JangHyukJin <wkdgurwls00@naver.com>
2019-11-26 16:19:26 +0900

    added recommendations

--- recommendation/views.py
+++ recommendation/views.py
@@ -1,6 +1,3 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-
-class QuestionFormTemplateView(TemplateView):
-    template_name = 'recommendation/question_form.html'
