a91938187fea7a81893a2454bec01e3feafc4a9b
JangHyukJin <wkdgurwls00@naver.com>
2019-11-26 16:16:31 +0900

    modified settings.py

--- OurSpecialPlace/settings.py
+++ OurSpecialPlace/settings.py
@@ -128,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/')
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
+
+LOGIN_REDIRECT_URL = '/choice/'
