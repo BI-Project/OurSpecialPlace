e8d09c19792276358d71cb370a76b0247112a067
JungChaeMoon <jungchaemoon@gmail.com>
2019-11-27 20:29:53 +0900

    Added user property

--- place/views.py
+++ place/views.py
@@ -11,6 +11,8 @@ from recommendation.src import Spot_list
 from recommendation.src.MakeResult import FunctionBox
 from CollaborativeFiltering.collaborative_filtering import CollaborativeFiltering
 
+from place.models import UserPlaceStar
+
 login_url = reverse_lazy('accounts:login')
 
 
@@ -43,9 +45,21 @@ class UserProfileReceiveView(View):
         result_list.append(int(request.POST.get('history_modern')))
         result_list.append(int(request.POST.get('save_flex')))
         result_list.append(int(request.POST.get('accessibility')))
+        result_list.append(int(request.POST.get('season')))
         result_list.append(int(request.POST.get('dormitory_hotel')))
         result_list.append(int(request.POST.get('day_N_night')))
         result_list.append(int(request.POST.get('age')))
+
+        request.user.natural_city = int(request.POST.get('natural_city'))
+        request.user.static_dynamic = int(request.POST.get('static_dynamic'))
+        request.user.mountain_sea = int(request.POST.get('mountain_sea'))
+        request.user.history_modern = int(request.POST.get('history_modern'))
+        request.user.save_flex = int(request.POST.get('save_flex'))
+        request.user.accessibility = int(request.POST.get('accessibility'))
+        request.user.dormitory_hotel = int(request.POST.get('dormitory_hotel'))
+        request.user.season = int(request.POST.get('season'))
+        request.user.day_N_night = int(request.POST.get('day_N_night'))
+        request.user.age = int(request.POST.get('age'))
 
         topten = FunctionBox(result_list, Spot_list.data_list)
         topten.CosSimilarity()
@@ -61,7 +75,6 @@ class UserProfileReceiveView(View):
 
 
 class UserStarReceiveView(View):
-
     def post(self, request, *args, **kwargs):
         pk = request.POST.get('pk')
         place_name = request.POST.get('name')
@@ -71,6 +84,15 @@ class UserStarReceiveView(View):
         place_star_dict = {}
         place_star_dict[place_name] = int(star)
         user_place_dict[user] = place_star_dict
+
+        place = get_object_or_404(Place, pk=pk)
+
+        user_place = UserPlaceStar(user=request.user, place=place, star=star)
+        user_place.save()
+
+        UserPlaceStar.objects.filter(place=place_name)
+
+
 
         collabo = CollaborativeFiltering(user_place_dict)
 
