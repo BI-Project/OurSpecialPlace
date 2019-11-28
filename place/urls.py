a8ee79d9ab134aefb6723306aee1d2a9a57daeb0
JungChaeMoon <jungchaemoon@gmail.com>
2019-11-29 02:57:04 +0900

    Updated url

--- place/urls.py
+++ place/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('choice/', PlaceChoiceTemplateView.as_view(), name='choice'),
     path('ListAPI', PlaceChoiceListView.as_view(), name='list_api'),
     path('thanks/', ThanksTemplateView.as_view(), name='thanks'),
-    path('UserProfileReceiveAPI', UserProfileReceiveView.as_view(), name='user_profile_receive'),
-    path('UserStarReceive', UserStarReceiveView.as_view(), name='user_star_receive'),
+    path('UserProfileReceiveAPI/', UserProfileReceiveView.as_view(), name='user_profile_receive'),
+    path('UserStarReceive/', UserStarReceiveView.as_view(), name='user_star_receive'),
 
 ]
