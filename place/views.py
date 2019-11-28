f756b4eb7ff07620a163bb437322b0fa487f787b
JungChaeMoon <jungchaemoon@gmail.com>
2019-11-28 16:56:08 +0900

    Updated view

--- place/views.py
+++ place/views.py
@@ -21,7 +21,7 @@ login_url = reverse_lazy('accounts:login')
 
 class PlaceChoiceListView(mixins.ListModelMixin, generics.GenericAPIView):
     serializer_class = TestPlaceSerializer
-    queryset = TestPlace.objects.all().order_by('?')[:20]
+    queryset = TestPlace.objects.all().order_by('?')
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
